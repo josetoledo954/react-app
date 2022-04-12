c2398e2d6e62a36845548babb5b65d7a4f086c93
josetoledo954 <josetoledo954@gmail.com>
2022-04-12 01:20:47 -0300

    react

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='NavBar'>
+      <h2>Tienda</h2>
+        <li>inicio</li>
+        <li>producto</li>
+        <li>preguntas</li>
+        <li>contacto</li>
+    </div>
+  )
+}
+
+export default NavBar
